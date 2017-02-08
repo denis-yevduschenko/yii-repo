3c56482755258531b2edff35b7bd5b41533f9b09
dyevduschenko <dyevduschenko>
2017-02-08 11:59:53 +0200

    edit category

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=3307;dbname=mysite-local',//my home db
-    //'dsn' => 'mysql:host=localhost;dbname=yii2_db',//my work db
+    //'dsn' => 'mysql:host=localhost;port=3307;dbname=mysite-local',//my home db
+    'dsn' => 'mysql:host=localhost;dbname=yii2_db',//my work db
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
--- views/post/category.php
+++ views/post/category.php
@@ -1,4 +1,6 @@
 <?php
+use app\assets\CategoryAsset;
+use kartik\icons\Icon;
 use yii\helpers\Html;
 
 $this->registerMetaTag([
@@ -15,28 +17,45 @@ if($category == 'all'){
     $this->params['breadcrumbs'][] = ['label' => 'All posts'];
 } else {
     $this->params['breadcrumbs'][] = Html::encode($posts[0]->category);
-}?>
+}
+
+CategoryAsset::register($this);?>
 
 <?foreach ($posts as $post){?>
     <? if($post->hide){
        continue;
     }?>
-    <div class="panel panel-primary">
-        <div class="panel-heading">
-            <h4><?=Html::encode($post->title)?><small class="bg-primary pull-right"><?=Html::encode($post->date)?></small></h4>
-        </div>
-        <div class="panel-body">
-            <div class="hidden-xs col-sm-3">
-                <?=Html::img($post->imagepath, ['alt' => $post->title, 'height' => 200, 'class' => 'img-responsive'])?>
+    <div class="col-xs-2">
+        <div class="panel panel-info">
+            <div class="panel-heading info-title">
+                <?=Html::encode($post->date)?>
             </div>
-            <div class="col-xs-12 col-sm-9">
-                <div class="col-xs-8">
-                    <?=Html::encode($post->intro_text)?>
+            <div class="panel-body info-block">
+                <p><?=Icon::show('user')?>Denis</p>
+                <p><?=Icon::show('comments-o')?>23 comments</p>
+                <p><?=Icon::show('book')?><?=$post->hits?> reads</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xs-10">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h4><?=Html::encode($post->title)?></h4>
+            </div>
+            <div class="panel-body content">
+                <div class="hidden-xs col-sm-3">
+                    <?=Html::img($post->imagepath, ['alt' => $post->title, 'height' => 200, 'class' => 'img-responsive'])?>
                 </div>
-                <div class="col-xs-4 pull-down">
-                    <div class="btn-group-vertical btn-block">
-                        <a href="<?=Yii::$app->urlManager->createUrl(['post/category', 'category' => $post->category])?>" class="btn btn-default" role="button"><?=$post->category?></a>
-                        <a href="<?=Yii::$app->urlManager->createUrl(['post/article', 'id' => $post->id])?>" class="btn btn-success" role="button">Read more <span class="badge"><?=$post->hits?></span></a>
+                <div class="col-xs-12 col-sm-9">
+                    <div class="col-xs-8">
+                        <?=Html::encode($post->intro_text)?>
+                    </div>
+                    <div class="col-xs-4 pull-down">
+                        <div class="btn-group-vertical btn-block">
+                            <a href="<?=Yii::$app->urlManager->createUrl(['post/category', 'category' => $post->category])?>" class="btn btn-default" role="button"><?=$post->category?></a>
+                            <a href="<?=Yii::$app->urlManager->createUrl(['post/article', 'id' => $post->id])?>" class="btn btn-success" role="button">Read more <span class="badge"><?=$post->hits?></span></a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -49,13 +68,13 @@ if($category == 'all'){
 <script>
     $('.pull-down').each(function () {
         var $this = $(this);
-        $this.css('margin-top', $('.panel-body').height() - $this.height());
+        $this.css('margin-top', $('.content').height() - $this.height());
     });
     $(window).resize(function (){
         $('.pull-down').each(function () {
             var $this = $(this);
             $this.css('display', 'none');
-            panelHeight = $('.panel-body').height();
+            panelHeight = $('.content').height();
             $this.css('display', 'block');
             if ((panelHeight - $this.height()) <= 0){
                 $this.css('margin-top', 0);
